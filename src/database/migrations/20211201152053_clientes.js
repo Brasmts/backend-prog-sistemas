1fbfa1b340549a233b7c9a61a3b194e75c5910c2
Brasmts <matheus-bras@hotmail.com>
2021-12-28 13:27:22 -0300

    Primeiro commit

---
+++ src/database/migrations/20211201152053_clientes.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+        return knex.schema.createTable('clientes', function (table) {
+             table.increments('id');
+             table.string('nome').notNullable();
+             table.string('sobrenome').notNullable();
+             table.string('cpf').notNullable().unique();
+             table.string('sexo', 2).notNullable();
+             table.string('endereco').nullable();
+             table.string('cidade').notNullable();
+             table.string('uf').nullable();
+             table.string('email').notNullable().unique();
+             table.string('celular').notNullable();
+           })
+     };
+     
+     exports.down = function(knex) {
+        return knex.schema.dropTable('clientes');
+     };
